70efd72a17736f183155d92735bfe4bad17929f8
Camilo Lopez <camilino123@gmail.com>
2022-02-07 17:41:08 -0500

    COntroller added and utils created

---
+++ src/main/java/com/isladellago/billmanager/util/LogUtils.java
@@ -0,0 +1,10 @@
+package com.isladellago.billmanager.util;
+
+public final class LogUtils {
+
+    public static final String HEALTH_LOG = "Service is healthy";
+
+    private LogUtils() {
+        throw new AssertionError();
+    }
+}
---
+++ src/main/java/com/isladellago/billmanager/util/PathUtils.java
@@ -0,0 +1,12 @@
+package com.isladellago.billmanager.util;
+
+public final class PathUtils {
+
+    public static final String BASE_PATH = "/api/v1/bill";
+
+    public static final String HEALTH_PATH = "/health";
+
+    private PathUtils() {
+        throw new AssertionError();
+    }
+}
