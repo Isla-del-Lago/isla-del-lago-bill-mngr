6a0c1066ab3e8bce7e1dae4d2a14e1fa767b8d69
Camilo Lopez <camilino123@gmail.com>
2022-05-05 21:27:08 -0500

    Some constraints added

--- src/main/java/com/isladellago/billmanager/domain/dto/CalculateConsumptionsPercentageBody.java
+++ src/main/java/com/isladellago/billmanager/domain/dto/CalculateConsumptionsPercentageBody.java
@@ -3,9 +3,13 @@ package com.isladellago.billmanager.domain.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
@@ -13,11 +17,15 @@ import java.util.List;
 @Builder
 public final class CalculateConsumptionsPercentageBody {
 
+    private static final int APARTMENTS_SIZE = 10;
+
     @NotNull
+    @Positive
     private final Integer billId;
 
     @NotNull
     @NotEmpty
-    private final List<ConsumptionInfo> consumptions;
+    @Size(min = APARTMENTS_SIZE, max = APARTMENTS_SIZE)
+    private final List<@Valid ConsumptionInfo> consumptions;
 }
 
---
+++ src/main/java/com/isladellago/billmanager/util/ConstantUtils.java
@@ -0,0 +1,7 @@
+package com.isladellago.billmanager.util;
+
+public final class ConstantUtils {
+
+    public static final String APARTMENT_REGEX = "((Apartamento\\s(201|202|301|302|401|402|501|502))|(Local\\s(1|2)))";
+    public static final String MIN_CONSUMPTION_VALUE = "0.0";
+}
