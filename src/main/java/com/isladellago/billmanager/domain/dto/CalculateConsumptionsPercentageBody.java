3ce0824830e86ff235e7036a171417a6ac92a5fd
Camilo Lopez <camilino123@gmail.com>
2022-06-24 19:00:49 -0500

    Default constructor added

--- src/main/java/com/isladellago/billmanager/domain/dto/CalculateConsumptionsPercentageBody.java
+++ src/main/java/com/isladellago/billmanager/domain/dto/CalculateConsumptionsPercentageBody.java
@@ -3,6 +3,7 @@ package com.isladellago.billmanager.domain.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public final class CalculateConsumptionsPercentageBody {
 
@@ -20,11 +22,11 @@ public final class CalculateConsumptionsPercentageBody {
 
     @NotNull
     @Positive
-    private final Integer billId;
+    private Integer billId;
 
     @NotNull
     @NotEmpty
     @Size(min = APARTMENTS_SIZE, max = APARTMENTS_SIZE)
-    private final List<@Valid ConsumptionInfo> consumptions;
+    private List<@Valid ConsumptionInfo> consumptions;
 }
 
